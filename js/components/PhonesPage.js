17ca69f868ce98f9f2881747dab1d42452f60edb
mgrinko <glukym@gmail.com>
2019-04-04 20:49:37 +0300

    PhoneViewer was added

--- js/components/PhonesPage.js
+++ js/components/PhonesPage.js
@@ -1,4 +1,5 @@
 import PhonesCatalog from './PhonesCatalog.js';
+import PhoneViewer from './PhoneViewer.js';
 import ShoppingCart from './ShoppingCart.js';
 import Filter from './Filter.js';
 import { getAll, getById } from '../api/phones.js';
@@ -7,10 +8,19 @@ export default class PhonesPage {
   constructor(element) {
     this.element = element;
 
+    this.state = {
+      phones: getAll(),
+      selectedPhone: null,
+    };
+
     this.render();
 
     this.initComponent(PhonesCatalog, {
-      phones: getAll(),
+      phones: this.state.phones,
+    });
+
+    this.initComponent(PhoneViewer, {
+      phone: this.state.selectedPhone,
     });
 
     this.initComponent(ShoppingCart);
@@ -18,10 +28,12 @@ export default class PhonesPage {
   }
 
   initComponent(Constructor, props = {}) {
-    new Constructor(
-      this.element.querySelector(`[data-component="${Constructor.name}"]`),
-      props
-    );
+    const componentName = Constructor.name;
+    const element = this.element.querySelector(`[data-component="${componentName}"]`);
+
+    if (element) {
+      new Constructor(element, props);
+    }
   }
 
   render() {
@@ -41,7 +53,11 @@ export default class PhonesPage {
   
         <!--Main content-->
         <div class="col-md-10">
-          <div data-component="PhonesCatalog"></div>
+          ${ this.state.selectedPhone ? `
+            <div data-component="PhoneViewer"></div>
+          ` : `
+            <div data-component="PhonesCatalog"></div>
+          `}
         </div>
       </div>
     `;
