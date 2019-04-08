b634ebc9b073db4ba41974e56ce140250eb53898
mgrinko <glukym@gmail.com>
2019-04-08 19:32:46 +0300

    on was added

--- js/components/PhonesCatalog.js
+++ js/components/PhonesCatalog.js
@@ -15,6 +15,24 @@ export default class PhonesCatalog {
       const phoneId = link.dataset.phoneId;
       this.props.onPhoneSelected(phoneId);
     });
+
+    this.on('click', 'PhoneLink', (event) => {
+      const phoneId = event.delegateTarget.dataset.phoneID;
+      this.props.onPhoneSelected(phoneId);
+    });
+  }
+
+  on(eventName, elementName, callback) {
+    this.element.addEventListener(eventName, (event) => {
+      const delegateTarget = event.target.closest(`[data-element="${elementName}"]`);
+
+      if (!delegateTarget) {
+        return;
+      }
+
+      event.delegateTarget = delegateTarget;
+      callback(event);
+    });
   }
 
   render() {
